913439b57d8fa43b631b2d45019048bd675ffc20
Matt A <matt.armata8@gmail.com>
2020-09-25 17:48:35 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -15,7 +15,7 @@ function moveMapToBerlin(map){
 //Step 1: initialize communication with the platform
 // In your own code, replace variable window.apikey with your own apikey
 var platform = new H.service.Platform({
-  apikey: window.apikey
+  apikey: "qyoocETQNJGdTCbCzgjS_lbtIDMzw3CHu48hUeIxvQI"
 });
 var defaultLayers = platform.createDefaultLayers();
 
